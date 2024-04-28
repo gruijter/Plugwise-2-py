93b50a3d78cdab48a8f55b758d2e8989bdef0962
Michel <155964154+vorenhoutgithub@users.noreply.github.com>
2024-04-28 17:49:17 +0200

    Revert Plugwise-2.py
    Needs to be edited in /usr/src/Plugwise-2-py/swutil/pwmqtt.py

--- Plugwise-2.py
+++ Plugwise-2.py
@@ -1456,10 +1456,9 @@ try:
     elif 'mqtt_ip' in cfg and 'mqtt_port' in cfg:
         #connect to server and start worker thread.
         if 'mqtt_user' in cfg and 'mqtt_password' in cfg:
-            # Need to explicitely define the API version, see https://stackoverflow.com/questions/77984857/paho-mqtt-unsupported-callback-api-version-error
-            mqttclient = Mqtt_client(Mqtt_client.CallbackAPIVersion.VERSION1, cfg['mqtt_ip'], cfg['mqtt_port'], qpub, qsub,"Plugwise-2-py",cfg['mqtt_user'],cfg['mqtt_password'])
+            mqttclient = Mqtt_client(cfg['mqtt_ip'], cfg['mqtt_port'], qpub, qsub,"Plugwise-2-py",cfg['mqtt_user'],cfg['mqtt_password'])
         else:
-            mqttclient = Mqtt_client(Mqtt_client.CallbackAPIVersion.VERSION1, cfg['mqtt_ip'], cfg['mqtt_port'], qpub, qsub, "Plugwise-2-py")
+            mqttclient = Mqtt_client(cfg['mqtt_ip'], cfg['mqtt_port'], qpub, qsub, "Plugwise-2-py")
         mqttclient.subscribe("plugwise2py/cmd/#")
         mqtt_t = threading.Thread(target=mqttclient.run)
         mqtt_t.setDaemon(True)
